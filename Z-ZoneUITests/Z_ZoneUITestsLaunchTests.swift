d8919e7aed7c0681dfced8f070da15eeecc8b34c
Mark Laramee <mlaramee@rccl.com>
2023-01-14 14:31:29 -0800

    Initial Commit

---
+++ Z-ZoneUITests/Z_ZoneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Z_ZoneUITestsLaunchTests.swift
+//  Z-ZoneUITests
+//
+//  Created by Mark Laramee on 1/14/23.
+//
+
+import XCTest
+
+final class Z_ZoneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
