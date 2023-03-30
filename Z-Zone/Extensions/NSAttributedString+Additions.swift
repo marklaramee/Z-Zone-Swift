984228c4575c7f1a7b4c4e36ed7b850681620e14
Mark Laramee <marklaramee@gmail.com>
2023-03-29 23:59:15 -0700

    added font styles and colors

--- Z-Zone/Extensions/NSAttributedString+Additions.swift
+++ Z-Zone/Extensions/NSAttributedString+Additions.swift
@@ -6,8 +6,43 @@
 //
 
 import Foundation
+import UIKit
 
 extension NSAttributedString {
+    convenience init(
+        zString: String,
+        size: CGFloat,
+        style: UIFont.ZFontStyle,
+        color: UIColor = UIColor.ZZone.black,
+        align: NSTextAlignment? = nil,
+        isAllCaps: Bool = false,
+        lineSpacing: CGFloat? = nil
+    ) {
+        // TODO: replace initializer below with a11yFontSize
+        // let a11yFontSize = AccessibilityUtil.shared.maxFontSize(size)
+        
+        let font = UIFont(zStyle: style, size: size)
+        let fontColor = color // TODO: remove?
+        let titleString = isAllCaps ? zString.uppercased() : zString
+        var attributes: [NSAttributedString.Key: Any] = [
+            .font: font,
+            .foregroundColor: fontColor
+        ]
+        
+        let style = NSMutableParagraphStyle()
+        var addParagraphStyle = false
+        if let alignment = align {
+            style.alignment = alignment
+            addParagraphStyle = true // TODO: refactor
+        }
+        if addParagraphStyle {
+            attributes[.paragraphStyle] = style
+        }
+        
+        self.init(string: titleString, attributes: attributes)
+    }
+    
+    
     /// Trims beginning and ending whitespace
     func trim() -> NSAttributedString {
         let invertedSet = CharacterSet.whitespacesAndNewlines.inverted
