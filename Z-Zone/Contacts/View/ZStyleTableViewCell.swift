b10e02029d49f33c769bf07a2323df57c076af3e
Mark Laramee <marklaramee@gmail.com>
2023-04-02 22:16:53 -0700

    removed table cell swift file and alpha adjustment on row bg

--- Z-Zone/Contacts/View/ZStyleTableViewCell.swift
+++ Z-Zone/Contacts/View/ZStyleTableViewCell.swift
@@ -17,6 +17,7 @@ class ZStyleTableViewCell: UITableViewCell {
         // Initialization code
     }
 
+    // TODO: delete
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
