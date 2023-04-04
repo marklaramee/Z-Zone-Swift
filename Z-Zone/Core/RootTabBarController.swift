e3d565857bf1638583be87eb7bf3170ef4d6abda
Mark Laramee <marklaramee@gmail.com>
2023-04-03 21:10:22 -0700

    added root tab bar controller with selected index

---
+++ Z-Zone/Core/RootTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  RootTabBarController.swift
+//  Z-Zone
+//
+//  Created by Mark Laramee on 4/3/23.
+//
+
+import UIKit
+
+class RootTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        selectedIndex = 1
+    }
+}
