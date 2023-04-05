3b8961aee0886a8bad5837894582946ee15de048
Mark Laramee <marklaramee@gmail.com>
2023-04-05 12:37:16 -0700

    changed access modifier for unit testing

--- Z-Zone/Contacts/ContactsClient.swift
+++ Z-Zone/Contacts/ContactsClient.swift
@@ -17,7 +17,7 @@ class ContactsClient {
     // TODO: convert to async
     // https://developer.apple.com/videos/play/wwdc2021/10194/?time=1290
     
-    private init() {
+    init() {
         switch (CNContactsUserDefaults.shared().sortOrder) {
         case .familyName:
             sortOrder = .familyName
