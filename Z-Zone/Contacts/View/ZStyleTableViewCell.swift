41126286624bca343540e0853149346efbafc29f
Mark Laramee <marklaramee@gmail.com>
2023-04-05 02:00:18 -0700

    bold zzone cxontacts for a11y

--- Z-Zone/Contacts/View/ZStyleTableViewCell.swift
+++ Z-Zone/Contacts/View/ZStyleTableViewCell.swift
@@ -13,14 +13,15 @@ class ZStyleTableViewCell: UITableViewCell {
     @IBOutlet weak var zImageView: UIImageView!
     
     func set(_ contact: ContactModel) {
-        let nameString = NSMutableAttributedString(zString: contact.fullName, size: 18, style: .almaraiRegular)
-        nameLabel.attributedText = nameString
-        
+        let nameString: NSMutableAttributedString
         if contact.isZZone {
+            nameString = NSMutableAttributedString(zString: contact.fullName, size: 18, style: .poppinsSemiBold)
             zImageView.isHidden = false
         } else {
+            nameString = NSMutableAttributedString(zString: contact.fullName, size: 18, style: .poppinsRegular)
             zImageView.isHidden = true
         }
+        nameLabel.attributedText = nameString
     }
 
 }
