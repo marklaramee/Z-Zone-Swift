45a3c9902ab509781ee2b01205de02b893621b27
marklaramee <marklaramee@gmail.com>
2023-04-06 14:10:42 -0700

    Prepare for release 2.0.0 (#12)
    * using contactModel for data
    
    * storyboard work for contacts view
    
    * table displaying with dumy data
    
    * setting name from model with trim
    
    * using contact names from the store
    
    * format
    
    * cleanup
    
    * moved contacts fetch to background queue
    
    * added logger utility
    
    * added string extension
    
    * updated remove and add methods to extension
    
    * generalized the extension
    
    * stubs for contact manipulation
    
    * moved store to class property
    
    * getting and using sort order
    
    * follow naming convention of store
    
    * executing successfully, not updating contact
    
    * successfully updating contact
    
    * moved full anme to contact model
    
    * changed to use custom sort order enum to reduce clutter in swith statements
    
    * populating model with zzone bool
    
    * visual indicator of zzone
    
    * succesfullyupdating contact and display
    
    * leaving zzone
    
    * display names without zzone string
    
    * info screen with scroll error
    
    * sorting contacts for display
    
    * cleanup
    
    * removed orphaned vc
    
    * fixed typo
    
    * phrasing
    
    * bar tint
    
    * reorder files
    
    * cleaned up vc extension
    
    * added manual instructions function
    
    * handling permissions deny notification
    
    * convert screen error to error label
    
    * show screen error when prompting to set permissions manually
    
    * fixed tab bar overlay issue
    
    * cover all a11y font sizes
    
    * a11y max font size
    
    * just use accessibility sizes
    
    * set scalar to float format for consistency
    
    * bold zzone cxontacts for a11y
    
    * changed access modifier for unit testing
    
    * updated comment
    
    * updated bg alpha
    
    * cleanup todos
    
    * updated app icon
    
    * prepping for launch
    
    * added contacts premission back to info.plist
    
    * updated info language

--- Z-Zone/Info/View/InfoViewController.swift
+++ Z-Zone/Info/View/InfoViewController.swift
@@ -16,15 +16,13 @@ class InfoViewController: UIViewController {
     
     let headerText = "Z-Zone"
     let bodyText = """
-    Do you have numbers in your phone that you can’t delete; but, never want to accidentally call? Like your landlord or an ex?
+    Do you have numbers in your phone that you can’t delete; but, never want to accidentally call? You need a Z-Zone.
     
-    You need a Z-Zone.
-
-    You create a Z-Zone by adding 3 z’s to the beginning of a contact’s name so they are pushed down to the end of your contacts list.
-
-    It's not hard for a couple of contacts; but, it can get tedious if you have many contacts to add or remove from your Z-Zone. The Z-Zone app makes this easy.
+    You create a Z-Zone by adding 3 z’s to the beginning of a contact’s name so they are pushed down to the end of your contacts list. It's not hard for a couple of contacts; but, it can get tedious for many contacts.
     
-    Simply go to the contacts tab and click a contact to move it in or out of your Z-Zone. A contact in your Z-Zone will appear with a purple “Z” background. When you leave the app and go to your phone’s contacts, you’ll see that all of your Z-Zone contacts are now safely tucked away at the bottom of the list.
+    The Z-Zone app makes this easy.
+    
+    Simply go to the contacts tab and click a contact to move it in or out of your Z-Zone. A contact in your Z-Zone will appear with a purple “Z” background. When you leave the app and go to your phone’s contacts, you’ll see that all of your Z-Zone contacts are now safely tucked away at the end of the list.
     """
     
     static func newInstance() -> InfoViewController {
